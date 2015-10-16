1053b8fa71d12a07dd00f0c80528bc3ba0bd156d
Knittel.Clara <Knittel.Clara@SK-D8CB8A509AF5>
2015-10-16 08:59:04 +0200

    new geometric objects

---
+++ src/animationGame/Circle.java
@@ -0,0 +1,16 @@
+package animationGame;
+
+public class Circle extends GeometricObject {
+
+	public Circle(double w,Vertex pos){super(w,w,pos);}
+	
+	public Circle(double w, double x, double y){super(w, w ,new Vertex(x,y));}
+	
+	public double area(){return (width/2)*(width/2)*Math.PI;}
+	
+	public String toString(){return "Circle("+super.toString()+")";}
+	
+	public boolean equals(Object that){return (that instanceof Circle && super.equals(that));}
+	
+	
+}
---
+++ src/animationGame/Triangle.java
@@ -0,0 +1,19 @@
+package animationGame;
+
+public class Triangle extends GeometricObject {
+	
+	public Triangle (double w,double h, Vertex pos){
+		super(w,h,pos);
+	}
+	public Triangle (double w,double h, double x, double y){
+		super(w,h, new Vertex (x,y));
+	}
+	
+	public double area(){return width*height/2;}
+	
+	public String toString(){return "Triangle(" + super.toString()+ ")";}
+	
+	public boolean equals(Object that){
+		return (that instanceof Triangle&& super.equals(that));
+	}
+}
