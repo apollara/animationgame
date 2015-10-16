44bce96a5c17a1465202f94b0da9d45172e498db
Knittel.Clara <Knittel.Clara@SK-D8CB8A509AF5>
2015-10-16 09:14:13 +0200

    test class

--- src/animationGame/Main.java
+++ src/animationGame/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		SimpleOval s1 = new SimpleOval(45,99,7,7);
+		Triangle t1	= new Triangle(66, 66,9, 8);
+		Circle c1 = new Circle(88,9,8);
+		
+		System.out.println(s1);
 	}
 
 }
