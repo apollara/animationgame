9b855487271785661c95418a52e97f6d66a005c9
Knittel.Clara <Knittel.Clara@SK-D8CB8A509AF5>
2015-10-20 14:42:25 +0200

    new Jframe

---
+++ src/animationGame/JFrameTest.java
@@ -0,0 +1,13 @@
+package animationGame;
+
+import javax.swing.JFrame;
+
+public class JFrameTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame f = new JFrame("Hier könnte ihre Werbung stehen");
+		f.setVisible(true);
+	}
+
+}
